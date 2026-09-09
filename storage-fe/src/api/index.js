3be82033a9256197187c9e9aaa0f9a7f3e6ee2c4
kwak-jagyung <dev.ripetangerine@gmail.com>
2026-09-09 17:06:09 +0900

    config(FE): vue file upload logic in progress

---
+++ storage-fe/src/api/index.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+const api = axios.create({
+  baseURL: baseURL,
+});
+
+// 요청 인터셉터 > 요청 나가기 전
+api.interceptors.request.use(
+  (config) => {
+    // 요청 전 수행
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// 응답 인터셉터 > 응답이 온 후 (직후)
+api.interceptors.response.use(
+  (response) => {
+    // 응답 가공 혹은 반환
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      const status = error.response.status;
+      console.error(`error occurred in 인터셉터, status: ${status}`);
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
---
+++ storage-fe/src/components/imageUploadPage.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from "vue";
+
+import api from "../api";
+
+const uploadedFile = ref(null);
+
+// TODO : 새로고침시 추가 로직 필요한지 확인
+onMounted(async () => {
+  try {
+    const res = await api.get("/image/upload");
+    const preUploadURL = res.uploadURL; // TODO : 추후 이곳에서 값이 없을 시 상세 오류가 나도록 설계
+  } catch (error) {
+    console.error(`error occurred: ${error}`);
+  }
+});
+
+async function handleImageUpload(e) {
+  //   this.input.image = this.$refs.images.files;
+  //   console.log(this.input.image);
+  if (uploadedFile.value) {
+    const formData = new FormData();
+    formData.append("uploadedFile", uploadedFile.value);
+  }
+}
+</script>
+
+/** 뇌를 활성화 그러니까 - presigned 클라이언트 로딩 시간에 >> 백엔드에게
+요청해서 업로드 가능한 url을 받고 만약 업로드가 되었다면 >> 해당 url로 이미지를
+업로드하고, 백엔드에는 그 url과 메타데이터를 주는거임 1. be url을 줘서 진행될
+인증의 방식 (sas)의 요청을 구현 2. fe url로 이미지를 보냄 3. fe 이미지 보낸게
+성공 했다면 be에게 이미지의 메타 데이터 전달 이후 저장 */
+
+<template>
+  <div>
+    <form method="get" @submit.prevent="handleImageUpload">
+      <label>image upload</label>
+      <input
+        @change="handleImageUpload"
+        ref="imageInput"
+        type="file"
+        accept="image/*"
+        id="image_upload"
+        name="image_upload"
+      />
+      <button>do it NOW!</button>
+    </form>
+
+    <div ref="preview" class="preview">
+      <p>{{ NO_DATA_TEXT }}</p>
+    </div>
+  </div>
+
+  <div class="uploaded_image_view">
+    <a :href="`/images/${image}`" target="_blank">{{ image }}</a>
+  </div>
+</template>
+
+<style scoped></style>
