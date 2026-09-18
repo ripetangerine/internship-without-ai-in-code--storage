5f7e80f13c4e7d93ce0e330b84a4ed92375a4e9e
ripetangerine <dev.ripetangerine@gmail.com>
2026-09-18 11:50:46 +0900

    wip(fe/be): 그래프와 서버 storage 로직 테스트

--- storage-be/WebApplication1/Infra/Repositories/UploadFileImageRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Infra.Repositories
-{
-    public class UploadFileImageRepository
-    {
-    }
-}
--- storage-be/WebApplication1/WebApplication1/Controllers/FilesController.cs
+++ storage-be/WebApplication1/WebApplication1/Controllers/FilesController.cs
@@ -15,19 +15,12 @@ public class FilesController : ControllerBase
     }
 
     [HttpPost("images")]
-    public async Task<IActionResult> UploadImageDirect([FromForm] IFormFile ProfileImage) // FromBody = 텍스트 전용
+    public async Task<IActionResult> UploadImageDirect(IFormFile ProfileImage) // FromBody = 텍스트 전용
     {
         // 프론트에서 사진을 보내주면 blob 형식으로 백엔드에서 바로 업로드 하는거임.
         // TODO : dto, service logic
         // TODO : 비지니스 로직 옮기기
-        //_blobRepo.UploadAsync()
-
-        //using var reader = new StreamReader(req.ProfileImage);
-        //var body = await reader.ReadToEndAsync();
-
-        //Console.WriteLine(body);
-        //return Ok();
-
+        // todo : 추가로 컨트롤러에서 챙겨야하는거 추가하기 (설계철학
         UploadImageResponse result = await _filesService.UploadImageFilesDirect(ProfileImage);
         return Ok(
             new
